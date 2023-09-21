dbc031c42bdb6c36b6c16027021e78ec002caff2
Rafay Memon <rafayrashid200611@gmail.com>
2023-09-21 16:04:24 +0500

    Server done post api working

--- routes/post.mjs
+++ routes/post.mjs
@@ -1,11 +1,14 @@
 import express from "express";
 import { customAlphabet } from "nanoid";
-// import { client } from "./../mongodb.mjs";
-// import { ObjectId } from "mongodb";
+import { client } from "./../mongodb.mjs";
+import { ObjectId } from "mongodb";
 import pineconeClient from "./../pinecone.mjs";
 import openaiClient from "./../openai.mjs";
 
 const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 10);
+
+const db = client.db("cruddb");
+const col = db.collection("posts");
 
 const pcIndex = pineconeClient.Index(process.env.PINECONE_INDEX_NAME);
 console.log(
@@ -31,19 +34,19 @@ router.post("/post", async (req, res, next) => {
   }
 
   try {
-    const insertResponse = await col.insertOne({
-      title: req.body.title,
-      text: req.body.text,
-      createdOn: new Date(),
-    });
-    console.log("insertResponse: ", insertResponse);
+    // const insertResponse = await col.insertOne({
+    //     // _id: "7864972364724b4h2b4jhgh42",
+    //     title: req.body.title,
+    //     text: req.body.text,
+    //     createdOn: new Date()
+    // });
+    // console.log("insertResponse: ", insertResponse);
 
     const response = await openaiClient.embeddings.create({
       model: "text-embedding-ada-002",
       input: `${req.body.title} ${req.body.text}`,
     });
     const vector = response?.data[0]?.embedding;
-
     console.log("vector: ", vector);
 
     const upsertResponse = await pcIndex.upsert([
@@ -67,14 +70,28 @@ router.post("/post", async (req, res, next) => {
 });
 
 router.get("/posts", async (req, res, next) => {
+  // const cursor = col.find({})
+  //     .sort({ _id: -1 })
+  //     .limit(100);
+
+  // try {
+  //     let results = await cursor.toArray()
+  //     console.log("results: ", results);
+  //     res.send(results);
+  // } catch (e) {
+  //     console.log("error getting data mongodb: ", e);
+  //     res.status(500).send('server error, please try later');
+  // }
+
   try {
     const response = await openaiClient.embeddings.create({
       model: "text-embedding-ada-002",
       input: "",
     });
     const vector = response?.data[0]?.embedding;
-
     console.log("vector: ", vector);
+    // [ 0.0023063174, -0.009358601, 0.01578391, ... , 0.01678391, ]
+
     const queryResponse = await pcIndex.query({
       vector: vector,
       // id: "vec1",
@@ -96,7 +113,6 @@ router.get("/posts", async (req, res, next) => {
       text: eachMatch?.metadata?.text,
       title: eachMatch?.metadata?.title,
       _id: eachMatch?.id,
-      time: new Date(eachMatch?.metadata?.createdOn).toLocaleString(),
     }));
 
     res.send(formattedOutput);
@@ -105,107 +121,6 @@ router.get("/posts", async (req, res, next) => {
     res.status(500).send("server error, please try later");
   }
 });
-
-router.get("/search", async (req, res, next) => {
-  try {
-    const response = await openaiClient.embeddings.create({
-      model: "text-embedding-ada-002",
-      input: req.query.q,
-    });
-    const vector = response?.data[0]?.embedding;
-    console.log("vector: ", vector);
-
-    const queryResponse = await pcIndex.query({
-      vector: vector,
-      topK: 20,
-      includeValues: false,
-      includeMetadata: true,
-    });
-
-    queryResponse.matches.map((eachMatch) => {
-      console.log(
-        `score ${eachMatch.score.toFixed(3)} => ${JSON.stringify(
-          eachMatch.metadata
-        )}\n\n`
-      );
-    });
-    console.log(`${queryResponse.matches.length} records found `);
-
-    const formattedOutput = queryResponse.matches.map((eachMatch) => ({
-      text: eachMatch?.metadata?.text,
-      title: eachMatch?.metadata?.title,
-      _id: eachMatch?.id,
-    }));
-
-    res.send(formattedOutput);
-  } catch (e) {
-    console.log("error getting data pinecone: ", e);
-    res.status(500).send("server error, please try later");
-  }
-});
-router.put("/post/:postId", async (req, res, next) => {
-  if (!req.body.text && !req.body.title) {
-    res.status(403)
-      .send(`required parameter missing, atleast one key is required.
-        example put body: 
-        PUT     /api/v1/post/:postId
-        {
-            title: "updated title",
-            text: "updated text"
-        }
-        `);
-  }
-
-  try {
-    const response = await openaiClient.embeddings.create({
-      model: "text-embedding-ada-002",
-      input: `${req.body.title} ${req.body.text}`,
-    });
-    const vector = response?.data[0]?.embedding;
-    console.log("vector: ", vector);
-
-    const upsertResponse = await pcIndex.upsert([
-      {
-        id: req.params.postId,
-        values: vector,
-        metadata: {
-          title: req.body.title,
-          text: req.body.text,
-        },
-      },
-    ]);
-    console.log("upsertResponse: ", upsertResponse);
-
-    res.send({ message: "post created" });
-  } catch (e) {
-    console.log("error inserting mongodb: ", e);
-    res.status(500).send({ message: "server error, please try later" });
-  }
-});
-
-router.delete("/post/:postId", async (req, res, next) => {
-  const deleteResponse = await pcIndex.deleteOne(req.params.postId);
-  console.log("deleteResponse: ", deleteResponse);
-
-  res.send("post deleted");
-});
-
-router.delete("/posts/all", async (req, res) => {
-  try {
-    const deleteResponse = await pcIndex.deleteAll();
-
-    if (deleteResponse.status === "ok") {
-      res.status(200).json({ message: "All posts deleted successfully." });
-    } else {
-      res.status(500).json({ message: "Failed to delete all posts." });
-    }
-  } catch (error) {
-    console.error("Error deleting all posts:", error);
-    res.status(500).json({ message: "Server error, please try later." });
-  }
-});
-
-// search
 
 router.get("/search", async (req, res, next) => {
   try {
@@ -245,6 +160,126 @@ router.get("/search", async (req, res, next) => {
     console.log("error getting data pinecone: ", e);
     res.status(500).send("server error, please try later");
   }
+});
+
+// [92133,92254, 92255 ]
+
+router.get("/post/:postId", async (req, res, next) => {
+  console.log("this is signup!", new Date());
+
+  if (!ObjectId.isValid(req.params.postId)) {
+    res.status(403).send(`Invalid post id`);
+    return;
+  }
+
+  // const cursor = col.find({ price: { $lte: 77 } });
+  // const cursor = col.find({
+  //     $or: [
+  //         { _id: req.params.postId },
+  //         { title: "dfsdf sdfsdf" }
+  //     ]
+  // })
+
+  try {
+    let result = await col.findOne({ _id: new ObjectId(req.params.postId) });
+    console.log("result: ", result); // [{...}] []
+    res.send(result);
+  } catch (e) {
+    console.log("error getting data mongodb: ", e);
+    res.status(500).send("server error, please try later");
+  }
+});
+
+// PUT     /api/v1/post/:postId
+// {
+//     title: "updated title",
+//     text: "updated text"
+// }
+
+router.put("/post/:postId", async (req, res, next) => {
+  // if (!ObjectId.isValid(req.params.postId)) {
+  //     res.status(403).send(`Invalid post id`);
+  //     return;
+  // }
+
+  if (!req.body.text && !req.body.title) {
+    res.status(403)
+      .send(`required parameter missing, atleast one key is required.
+        example put body: 
+        PUT     /api/v1/post/:postId
+        {
+            title: "updated title",
+            text: "updated text"
+        }
+        `);
+  }
+
+  // let dataToBeUpdated = {};
+  // if (req.body.title) { dataToBeUpdated.title = req.body.title }
+  // if (req.body.text) { dataToBeUpdated.text = req.body.text }
+  // try {
+  //     const updateResponse = await col.updateOne(
+  //         {
+  //             _id: new ObjectId(req.params.postId)
+  //         },
+  //         {
+  //             $set: dataToBeUpdated
+  //         });
+  //     console.log("updateResponse: ", updateResponse);
+
+  //     res.send('post updated');
+  // } catch (e) {
+  //     console.log("error inserting mongodb: ", e);
+  //     res.status(500).send('server error, please try later');
+  // }
+
+  try {
+    const response = await openaiClient.embeddings.create({
+      model: "text-embedding-ada-002",
+      input: `${req.body.title} ${req.body.text}`,
+    });
+    const vector = response?.data[0]?.embedding;
+    console.log("vector: ", vector);
+
+    const upsertResponse = await pcIndex.upsert([
+      {
+        id: req.params.postId,
+        values: vector,
+        metadata: {
+          title: req.body.title,
+          text: req.body.text,
+        },
+      },
+    ]);
+    console.log("upsertResponse: ", upsertResponse);
+
+    res.send({ message: "post created" });
+  } catch (e) {
+    console.log("error inserting mongodb: ", e);
+    res.status(500).send({ message: "server error, please try later" });
+  }
+});
+
+// DELETE  /api/v1/post/:postId
+router.delete("/post/:postId", async (req, res, next) => {
+  // if (!ObjectId.isValid(req.params.postId)) {
+  //     res.status(403).send(`Invalid post id`);
+  //     return;
+  // }
+
+  // try {
+  //     const deleteResponse = await col.deleteOne({ _id: new ObjectId(req.params.postId) });
+  //     console.log("deleteResponse: ", deleteResponse);
+  //     res.send('post deleted');
+  // } catch (e) {
+  //     console.log("error deleting mongodb: ", e);
+  //     res.status(500).send('server error, please try later');
+  // }
+
+  const deleteResponse = await pcIndex.deleteOne(req.params.postId);
+  console.log("deleteResponse: ", deleteResponse);
+
+  res.send("post deleted");
 });
 
 export default router;
